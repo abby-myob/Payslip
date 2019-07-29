b0b3bbce6b19c073b20bd2edd245bec3cd046524
Abby Thompson <abby.thompson@c02tq0bphtd8.myob.myobcorp.net>
2019-07-30 11:45:30 +1200

    adaptation to add csv file and output a csv file with the payslips

--- PayslipsFirstSolution/PayslipsFirstSolution/Program.cs
+++ PayslipsFirstSolution/PayslipsFirstSolution/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace PayslipsFirstSolution
 {
@@ -6,30 +9,61 @@ namespace PayslipsFirstSolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Constants.Welcome);
-            var p = new Printer();
+
+            string filepath = "/Users/abby.thompson/Development/sample_input.csv";
+            string outputFilePath = "/Users/abby.thompson/Development/output.csv";
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+            List<string> output = new List<string>();
+            List<Person> people = new List<Person>();
+
+            lines.RemoveAt(0); //remove first line
             
-            var person = new Person
+            //create people and create calculate their payslip values
+            foreach (string line in lines)
             {
-                FirstName = p.StringInputValidation(Constants.FirstNameInput),
-                LastName = p.StringInputValidation(Constants.LastNameInput),
-                AnnualSalary = p.NumberInputValidation(Constants.AnnualSalaryInput),
-                SuperRate = p.NumberInputValidation(Constants.SuperRateInput),
-                PaymentStartDate = p.DateInputValidation(Constants.PaymentStartDateInput),
-                PaymentEndDate = p.DateInputValidation(Constants.PaymentEndDateInput),
-           };
+                string[] data = line.Split(",");
+                if (data.Length != 5) continue;
+                
+                Person person = new Person
+                {
+                    FirstName = data[0],
+                    LastName = data[1],
+                    AnnualSalary = int.Parse(data[2]),
+                    SuperRate = int.Parse(data[3].TrimEnd( new char[] { '%', ' ' } ) ),
+                    PayPeriod = data[4]
+                };
+                
+                PayslipLogic payslip = new PayslipLogic(person);
+                
+                payslip.CalculateFullName(); 
+                payslip.CalculateGrossIncome();
+                payslip.CalculateIncomeTax();
+                payslip.CalculateNetIncome();
+                payslip.CalculateSuper();
+                
+                people.Add(person);
+            }
             
-            PayslipLogic payslip = new PayslipLogic(person);
+
+            output.Add("name,pay period,gross income,income tax,net income,super");
             
-            payslip.CalculateFullName();
-            payslip.CalculatePayPeriod();
-            payslip.CalculateGrossIncome();
-            payslip.CalculateIncomeTax();
-            payslip.CalculateNetIncome();
-            payslip.CalculateSuper();
-
-
-            p.PrintPayslip(person);
+            // write back to the csv file
+            foreach (Person person in people)
+            {
+                output.Add(
+                    $"{person.FullName}," +
+                    $"{person.PayPeriod}," +
+                    $"{person.GrossIncome}," +
+                    $"{person.IncomeTax}," +
+                    $"{person.NetIncome}," +
+                    $"{person.Super}");
+            }
+            
+            File.WriteAllLines(outputFilePath, output);
         }
     }
+    
+    
+    
+    
 }
